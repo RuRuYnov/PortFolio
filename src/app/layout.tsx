316d04c400c669db7aa3126b1bf43fff960d49b5
DURIEZ Ruben <ruben.duriez@ynov.com>
2026-01-21 10:05:01 +0100

    [~] Cleaning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";  // <--- C'EST CETTE LIGNE QUI EST SOUVENT MANQUANTE
+import "./globals.css";  
 
 const inter = Inter({ subsets: ["latin"] });
 
